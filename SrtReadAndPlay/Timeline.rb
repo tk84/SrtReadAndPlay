2c4f44053c0bb029f159439fd8eb96aa1ec8d801
tk84 <git@tk84.net>
2011-10-24 18:39:37 +0900

    sqliteに切り替えほぼ完了

--- SrtReadAndPlay/Timeline.rb
+++ SrtReadAndPlay/Timeline.rb
@@ -26,11 +26,12 @@ class TimelineController < NSViewController
   end
 
   def tableView aTableView, objectValueForTableColumn:aTableColumn, row:rowIndex
-    @model.getOne aTableColumn.identifier, rowIndex
+#    @model.getOne aTableColumn.identifier, rowIndex
+    @model.label aTableColumn.identifier, rowIndex
   end
 
   def numberOfRowsInTableView aTableView
-    @model.numberOfRows
+    @model.count
   end
 
   def selectRow sender
@@ -39,15 +40,16 @@ class TimelineController < NSViewController
 end
 
 class TimelineModel
-  def initialize table
-    @table = table
+  def initialize records
+#    @table = table
 
     @dbFile = NSTemporaryDirectory() + MyFunction.uniqid + '.db'
     @db = SQLite3::Database.new(@dbFile)
     p @dbFile
 
     create_table
-    insert_into
+#    insert_into
+    records.each {|record| insert_into record }
   end
 
   def finalize
@@ -56,26 +58,88 @@ class TimelineModel
     File.delete @dbFile if @dbFile
   end
 
+  def time_format ftime
+    h = ftime / 3600
+    ftime %= 3600
+    m = ftime / 60
+    ftime %= 60
+    s = ftime
+    ftime -= ftime.truncate
+
+    '%02d:%02d:%02d,%03d' % [h,m,s,(ftime*1000)]
+  end
+
+  def label id, index
+    case id
+    when 'beginLabel'
+      time_format one(:btime, index)
+#       time_format @db.get_first_value(<<'EOF', index:index)
+# SELECT begin_time FROM master LIMIT :index, 1
+# EOF
+    when 'endLabel'
+      time_format one(:etime, index)
+#       time_format @db.get_first_value(<<'EOF', index:index)
+# SELECT end_time FROM master LIMIT :index, 1
+# EOF
+    when 'textLabel'
+#       caption =  @db.get_first_value(<<'EOF', index:index)
+# SELECT caption FROM master LIMIT :index, 1
+# EOF
+      one(:caption, index).
+        # gsub(/(<[^>]*>|\s)/, ' ').
+        gsub(/\s+/, ' ').
+        gsub(/(^\s|\s$)/, '')
+    else
+      'unknown'
+    end
+  end
+
+  def one id, index
+    row(index).fetch(id.to_s, 'unknown')
+  end
+
+  def row index
+    columns, row, *kipple = @db.execute2 <<'EOF', index:index
+SELECT
+  begin_time AS btime,
+  end_time AS etime,
+  caption
+FROM master
+LIMIT :index, 1
+EOF
+
+# #    puts "index:#{index}, columns:#{columns}, rows:#{rows}"
+     Hash[*[columns,row].transpose.flatten(1)]
+  end
+
+  def count
+    @db.get_first_value <<'EOF'
+SELECT count(sequence) FROM master;
+EOF
+  end
+
   def create_table
     @db.execute <<'EOF'
 CREATE TABLE master (
   sequence INTEGER,
   begin_time REAL,
-  end_time REAL
+  end_time REAL,
+  caption TEXT
 );
 EOF
   end
 
-  def insert_into seq, stime, etime
-    @db.execute(<<'EOF', {seq:seq, stime:stime, etime:etime})
-INSERT INTO master (sequence, begin_time, end_time)
-  VALUES (:seq, :stime, :etime)
+  def insert_into record
+    @db.execute <<'EOF', record
+INSERT INTO master VALUES (:seq, :btime, :etime, :caption);
 EOF
   end
 
   def region tableView
     indexSet = tableView.selectedRowIndexes
-    [@table[:btime][indexSet.firstIndex], @table[:etime][indexSet.lastIndex]]
+    [one(:btime, indexSet.firstIndex), one(:etime, indexSet.lastIndex)]
+
+#    [@table[:btime][indexSet.firstIndex], @table[:etime][indexSet.lastIndex]]
   end
 
   def self.makeModel url
@@ -85,6 +149,7 @@ EOF
       File.open url.path, 'r' do |file|
         # table = {btime:[], etime:[], text:[], bmsec:[], emsec:[],
         #   beginLabel:[], endLabel:[], textLabel:[], seq:[]}
+        records = []
         section = ''
 
         file.each_line do |line|
@@ -98,32 +163,44 @@ EOF
 
           if line =~ /^(\r\n|\n)/ then
             if section =~ /(?:^|\r?\n)(\d+)\r?\n(\d{2}):(\d{2}):(\d{2}),(\d{3}) --> (\d{2}):(\d{2}):(\d{2}),(\d{3})\r?\n(.*)/m then
-              table[:seq].push Regexp.last_match 1
-              table[:btime].
-                push((Regexp.last_match(2).to_f * 60 * 60) +
-                (Regexp.last_match(3).to_f * 60) +
-                (Regexp.last_match(4).to_f * 1) +
-                (Regexp.last_match(5).to_f / 1000))
-              table[:etime].
-                push((Regexp.last_match(6).to_f * 60 * 60) +
-                (Regexp.last_match(7).to_f * 60) +
-                (Regexp.last_match(8).to_f * 1) +
-                (Regexp.last_match(9).to_f / 1000))
-              table[:beginLabel].
-                push(Regexp.last_match(2) + ':' +
-                Regexp.last_match(3) + ':' +
-                Regexp.last_match(4) + '.' +
-                Regexp.last_match(5))
-              table[:endLabel].
-                push(Regexp.last_match(6) + ':' +
-                Regexp.last_match(7) + ':' +
-                Regexp.last_match(8) + '.' +
-                Regexp.last_match(9))
-              table[:textLabel].
-                push(Regexp.last_match(10).chomp.
-  #gsub(/(<[^>]*>|\s)/, ' ').
-                gsub(/\s+/, ' ').
-                gsub(/(^\s|\s$)/, ''))
+              records << {
+                seq:Regexp.last_match(1).to_i,
+                btime:((Regexp.last_match(2).to_f * 60 * 60) +
+                  (Regexp.last_match(3).to_f * 60) +
+                  (Regexp.last_match(4).to_f * 1) +
+                  (Regexp.last_match(5).to_f / 1000)),
+                etime:((Regexp.last_match(6).to_f * 60 * 60) +
+                  (Regexp.last_match(7).to_f * 60) +
+                  (Regexp.last_match(8).to_f * 1) +
+                  (Regexp.last_match(9).to_f / 1000)),
+                caption:Regexp.last_match(10).chomp
+              }
+
+  #             table[:btime].
+  #               push((Regexp.last_match(2).to_f * 60 * 60) +
+  #               (Regexp.last_match(3).to_f * 60) +
+  #               (Regexp.last_match(4).to_f * 1) +
+  #               (Regexp.last_match(5).to_f / 1000))
+  #             table[:etime].
+  #               push((Regexp.last_match(6).to_f * 60 * 60) +
+  #               (Regexp.last_match(7).to_f * 60) +
+  #               (Regexp.last_match(8).to_f * 1) +
+  #               (Regexp.last_match(9).to_f / 1000))
+  #             table[:beginLabel].
+  #               push(Regexp.last_match(2) + ':' +
+  #               Regexp.last_match(3) + ':' +
+  #               Regexp.last_match(4) + '.' +
+  #               Regexp.last_match(5))
+  #             table[:endLabel].
+  #               push(Regexp.last_match(6) + ':' +
+  #               Regexp.last_match(7) + ':' +
+  #               Regexp.last_match(8) + '.' +
+  #               Regexp.last_match(9))
+  #             table[:textLabel].
+  #               push(Regexp.last_match(10).chomp.
+  # #gsub(/(<[^>]*>|\s)/, ' ').
+  #               gsub(/\s+/, ' ').
+  #               gsub(/(^\s|\s$)/, ''))
             end
             section = ''
           else
@@ -131,19 +208,11 @@ EOF
           end
         end
 
-        model = self.new table if table[:textLabel].count >= 1
+          model = self.new records if records.count
       end
     end
 
     model
-  end
-
-  def getOne hash, index
-    @table[hash.to_sym][index]
-  end
-
-  def numberOfRows
-    @table.fetch(@table.keys.first, []).count
   end
 end
 
